dab6961fad99a6702a00811a66da61aeb7c64589
akiortagem <akiortagem@gmail.com>
2016-04-11 19:19:24 +0700

    new post redirects

--- blog/views/__init__.py
+++ blog/views/__init__.py
@@ -1,7 +1,7 @@
 from view_index import *
 from view_post import *
 from new_post import *
-from edit_post import *
+# from edit_post import *
 from login import *
 from list_post import *
 from contact import *
